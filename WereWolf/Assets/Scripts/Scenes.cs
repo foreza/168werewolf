348613ce9ad47bd722123ac2c46b2a27d51d20be
Connor Stokes <stokesc@uci.edu>
2015-04-10 21:09:20 -0700

    Added Scenes and Tags for real

---
+++ WereWolf/Assets/Scripts/Scenes.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scenes : MonoBehaviour 
+{
+    //For testing! CURRENTLY SHARED; DANGEROUS
+    string TESTSCENE = "TestScene";
+    //Connor's Scene for testing
+    string CONNORSCENE = "ConnorScene";
+}
---
+++ WereWolf/Assets/Scripts/Tags.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tags : MonoBehaviour 
+{
+    //Human Players
+    string HUMAN = "Human";
+
+    //Werewolf Players (Possibly bots)
+    string WEREWOLF = "Werewolf";
+
+    //Traps that can harm humans
+    string TRAP = "Trap";
+
+    //Terrain that NO ONE can pass
+    string HARDTERRAIN = "HardTerrain";
+
+    //Terrain that WEREWOLVES can pass
+    string SOFTTERRAIN = "SoftTerrain";
+
+    //Towers can potentially be activated to activate the victory zone
+    string TOWER = "Tower";
+
+    //HUMANS ONLY. Werewolves cannot enter
+    string HAVEN = "Haven";
+
+    //If a human reaches here, they win.
+    string VICTORYZONE = "VictoryZone";
+	
+}
