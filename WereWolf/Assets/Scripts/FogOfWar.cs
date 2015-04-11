a8b8e4f822de383d85dbdfa1061a146846fb3527
Jason Chiu <jforeza@gmail.com>
2015-04-11 10:25:11 -0700

    Werewolf and bushes in test scene now work as intended

--- WereWolf/Assets/Scripts/FogOfWar.cs
+++ WereWolf/Assets/Scripts/FogOfWar.cs
@@ -3,42 +3,33 @@ using System.Collections;
 
 public class FogOfWar : MonoBehaviour {
 
-
-	public float distanceToSee;
 	public bool visible;
-	public SpriteRenderer thisRenderer;
-	public Sprite thisSprite;
+
 
 	// Use this for initialization
 	void Start () {
-
-		thisRenderer = this.gameObject.GetComponent<SpriteRenderer>();
-		distanceToSee = 10.0f;
-		visible = false;
-
 	
+		setInvisible ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-
-		thisRenderer.color = new Color (1f, 1f, 1f, 0f);
-	
 	}
 
 
 	void setInvisible()
 		    {
 			print ("hiding");
-			thisRenderer.color = new Color (1f, 1f, 1f, 0f);
+			SpriteRenderer rend = this.gameObject.GetComponent<SpriteRenderer>();
+			rend.color = new Color (1f, 1f, 1f, 0f);
 			visible = false;
 		}
 
 		void setVisible()
 		{
-;
-		thisRenderer.color = new Color (1f, 1f, 1f, 1f);
+
+		SpriteRenderer rend = this.gameObject.GetComponent<SpriteRenderer>();
+		rend.color = new Color (1f, 1f, 1f, 1f);
 		visible = true;
 		print (this.gameObject.name + " is visible!");
 
