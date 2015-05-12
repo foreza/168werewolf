9b6f171f452d7800bc716adad0b9fce6b7abb915
Connor Stokes <stokesc@uci.edu>
2015-05-12 11:08:23 -0700

    Beginning work on StateManager

---
+++ WereWolf/Assets/Scripts/StateManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StateManager : MonoBehaviour {
+
+    Dictionary<int, PlayerController> humanDict = new Dictionary<int, PlayerController>();
+
+	// Use this for initialization
+	void Start () 
+    {
+        foreach (GameObject human in GameObject.FindGameObjectsWithTag(Tags.HUMAN))
+        {
+            PlayerController humanController = human.GetComponent<PlayerController>();
+
+            humanDict[humanController.playerID] = humanController;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void refreshAllStates()
+    {
+
+    }
+
+    public void humanPlayerStates()
+    {
+
+    }
+}
