a57ee475118a7e859f4b59e52f57814e586ed740
Connor Stokes <stokesc@uci.edu>
2015-05-23 13:05:30 -0700

    Humans -> Wolves (L Alt), Controllers Split Up
    Humans can now transform into werewolves (currently by pressing left
    alt). Minimap reflects changes (werewolves can only see werewolves,
    humans can only see humans)
    
    Playercontroller has had human-specific functionality split into its own
    class. Werewolfcontroller has been created, also as a child of player
    controller.

---
+++ WereWolf/Assets/Scripts/Game/WerewolfController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WerewolfController : PlayerController {
+
+    public Sprite minimapIcon;
+
+
+	// Use this for initialization
+	override protected void Start () 
+    {
+        base.Start();
+
+        if (!isClientControlled)
+        {
+            //For more information on the following, see this answer:
+            //http://answers.unity3d.com/questions/348974/edit-camera-culling-mask.html
+            GameObject.Find("Minimap Camera").GetComponent<Camera>().cullingMask |= 1 << LayerMask.NameToLayer("MapUIWerewolf"); //Display Werewolf-Only UI elements on the minimap
+            GameObject.Find("Minimap Camera").GetComponent<Camera>().cullingMask &= ~(1 << LayerMask.NameToLayer("MapUIHuman")); //Remove Human-Only UI elements from the minimap
+        }
+
+        GameObject minimapIconObject = transform.Find("MiniMap Icon").gameObject;
+        minimapIconObject.layer = 10; //LAYER 10 == MAPUIWEREWOLF ------- SHOULD NOT BE A MAGIC NUMBER ------- CHANGE LATER
+        minimapIconObject.GetComponent<SpriteRenderer>().sprite = minimapIcon;
+
+        transform.Find("Lantern Light").gameObject.SetActive(false);
+        currSprite.sprite = downSprite;
+	}
+	
+	// Update is called once per frame
+	override protected void Update () 
+    {
+        base.Update();	
+	}
+}
